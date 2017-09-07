f8edb184a96aeca4fadf2aeb324450b8766866c5
Victor Vieira <victorvieira302@gmail.com>
2017-09-07 14:45:53 -0300

    removed unused const
    Signed-off-by: Victor Vieira <victorvieira302@gmail.com>

--- app/src/main/java/com/vctapps/bustracker/core/BoardDefaults.kt
+++ app/src/main/java/com/vctapps/bustracker/core/BoardDefaults.kt
@@ -4,24 +4,15 @@ import android.os.Build
 
 object BoardDefaults {
 
-    val ID_MODULE_VALUE = 1
+    val ID_MODULE_VALUE = 136
 
     private val DEVICE_EDISON = "edison"
-    private val DEVICE_JOULE = "joule"
     private val DEVICE_RPI3 = "rpi3"
-    private val DEVICE_IMX6UL_PICO = "imx6ul_pico"
-    private val DEVICE_IMX6UL_VVDN = "imx6ul_iopb"
-    private val DEVICE_IMX7D_PICO = "imx7d_pico"
-
     val uartName: String
         get() {
             when (Build.DEVICE) {
                 DEVICE_EDISON -> return "UART1"
-                DEVICE_JOULE -> return "UART1"
                 DEVICE_RPI3 -> return "UART0"
-                DEVICE_IMX6UL_PICO -> return "UART3"
-                DEVICE_IMX6UL_VVDN -> return "UART2"
-                DEVICE_IMX7D_PICO -> return "UART6"
                 else -> throw IllegalStateException("Unknown Build.DEVICE " + Build.DEVICE)
             }
         }
